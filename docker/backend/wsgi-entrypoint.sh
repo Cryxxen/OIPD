588d6304c0a5268f3082500fac3372dd2c926de2
Artur <artnyr2004@gmail.com>
2022-10-12 19:16:28 +0600

    add docker

---
+++ docker/backend/wsgi-entrypoint.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+until [ cd app ]
+do
+   echo "Waiting for server volume..."
+done
+
+do
+    echo "Waiting for db to be ready..."
+    sleep 2
+done
+
+./manage.py collectstatic --noinput
+gunicorn OIPD.wsgi --bind 0.0.0.0:8000 --workers 4 --threads 2
