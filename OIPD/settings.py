9f737818b418d5caf7d077747f665be40a7262a5
Artur <artnyr2004@gmail.com>
2022-10-12 20:46:11 +0600

    second some changes

--- OIPD/settings.py
+++ OIPD/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
     "rest_framework",
     'drf_yasg',
+    "corsheaders",
     'mptt',
 
     # apps
@@ -62,6 +63,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -159,3 +161,5 @@ REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
     'PAGE_SIZE': 5
 }
+
+CORS_ALLOW_ALL_ORIGINS = True
