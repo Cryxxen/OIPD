766798a8b354023de41ffcc0b5a81fbd2b347540
Artur <artnyr2004@gmail.com>
2022-10-12 20:14:11 +0600

    second some changes

--- docker/backend/wsgi-entrypoint.sh
+++ docker/backend/wsgi-entrypoint.sh
@@ -1,14 +1,16 @@
 #!/bin/sh
 
-until [ cd app ]
+until cd /app/
 do
-   echo "Waiting for server volume..."
+    echo "Waiting for server volume..."
 done
 
+until ./manage.py migrate
 do
     echo "Waiting for db to be ready..."
     sleep 2
 done
 
 ./manage.py collectstatic --noinput
+
 gunicorn OIPD.wsgi --bind 0.0.0.0:8000 --workers 4 --threads 2
